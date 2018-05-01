e2bee6132713a2f8f909d2bebc6c0174311069d1
Isaac Rosenberg <irosenb7@gmail.com>
2018-04-30 19:46:48 -0700

    added skeleton.css, onto second page of donation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Isaac Rosenberg for Supervisor 2018', subtitle: null });
 });
 
+router.get('/donate', function(req, res, next) {
+  res.render('donate', { title: 'READY TO ACT?', amount: 'Amount', placeholder: 'How much is CHANGE worth to YOU?', amounts: [25, 50, 100, 250, 500], contribution_limit: 500, currency: "USD"})
+})
+
 module.exports = router;
